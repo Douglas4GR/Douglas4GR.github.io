4f6390b65c1d8d39a9d9c63cfceb3c72fe86164b
Douglas Garcia Rangel <douglasgovmad@gmail.com>
2024-11-11 01:34:48 -0300

    alterações sutis no layourt de cores

--- script.js
+++ script.js
@@ -10,25 +10,31 @@ document.addEventListener("DOMContentLoaded", function () {
         const jumbotron = document.querySelector(".jumbotron");
         const footer = document.querySelector("footer");
         const em = document.querySelector("em");
+        const logo = document.querySelector(".logo");
 
         // Configurações de cores para cada tema
         const colorSettings = {
-            light: { navbar: "navbar-light bg-light", jumbotron: "bg-light", footer: "bg-light", metaColor: "#ffffff", em: "destaque-blackwhite"},
-            dark: { navbar: "navbar-dark bg-dark", jumbotron: "bg-dark", footer: "bg-dark", metaColor: "#333333", em: "destaque-blackwhite" },
-            champagne: { navbar: " navbar-champagne", jumbotron: "jumbotron-champagne", footer: "footer-champagne", metaColor: "#99582A", em: "destaque-champagne" },
-            coast: { navbar: "navbar-light navbar-coast", jumbotron: "jumbotron-coast", footer: "footer-coast", metaColor: "#5E503F", em: "destaque-coast" },
-            taiga: { navbar: "navbar-dark navbar-taiga", jumbotron: "jumbotron-taiga", footer: "footer-taiga", metaColor: "#BC6C25", em: "destaque-taiga" },
+            light: { navbar: "navbar-light bg-light", jumbotron: "bg-light", footer: "bg-dark", metaColor: "#ffffff", em: "destaque-blackwhite", logo: "logo-blackwhite" },
+            dark: { navbar: "navbar-dark bg-dark", jumbotron: "bg-dark", footer: "bg-dark", metaColor: "#333333", em: "destaque-blackwhite", logo: "logo-blackwhite" },
+            champagne: { navbar: "navbar-champagne navbar-dark", jumbotron: "jumbotron-champagne", footer: "footer-champagne", metaColor: "#99582A", em: "destaque-champagne", logo: "logo-champagne" },
+            coast: { navbar: "navbar-light navbar-coast", jumbotron: "jumbotron-coast", footer: "footer-coast", metaColor: "#5E503F", em: "destaque-coast", logo: "logo-coast" },
+            taiga: { navbar: "navbar-dark navbar-taiga", jumbotron: "jumbotron-taiga", footer: "footer-taiga", metaColor: "#BC6C25", em: "destaque-taiga", logo: "logo-champagne" },
         };
 
         const settings = colorSettings[theme];
 
         if (settings) {
+            // Modificando as classes da navbar, jumbotron, footer, e outras
             navbar.className = `navbar navbar-expand-lg ${settings.navbar}`;
             jumbotron.className = `jumbotron text-center ${settings.jumbotron}`;
             footer.className = `text-center py-4 ${settings.footer}`;
             metaThemeColor.setAttribute("content", settings.metaColor);
             activeModeText.textContent = theme.charAt(0).toUpperCase() + theme.slice(1);
             em.className = `${settings.em}`;
+
+            // Alterando a classe do logo
+            logo.classList.remove("logo-blackwhite", "logo-champagne", "logo-coast", "logo-taiga"); // Remove todas as classes antigas de logo
+            logo.classList.add(settings.logo); // Adiciona a nova classe de logo
         }
     }
 
